a1d3f1a47f997b66bf373bc60b4a4192a5e52e7a
D3monizer <vysmol@gmail.com>
2023-12-17 20:25:00 +0300

    feat(plugin): add telescope-file-browser

--- init.lua
+++ init.lua
@@ -41,7 +41,6 @@ return {
       },
       disabled = { -- disable formatting capabilities for the listed language servers
         -- disable lua_ls formatting capability if you want to use StyLua to format your lua code
-        -- "lua_ls",
       },
       timeout_ms = 1000, -- default format timeout
       -- filter = function(client) -- fully override the default formatting function
@@ -69,6 +68,7 @@ return {
   -- augroups/autocommands and custom filetypes also this just pure lua so
   -- anything that doesn't fit in the normal config locations above can go here
   polish = function()
+    require("telescope").load_extension "file_browser"
     -- Set up custom filetypes
     -- vim.filetype.add {
     --   extension = {
